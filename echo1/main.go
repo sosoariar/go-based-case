bb67959c4c75c4322f6b92d1671394f3343ff47d
LuckilyChensy <LuckilyChensy@163.com>
2021-12-31 22:04:11 +0800

    for 循环形式1

---
+++ echo1/main.go
@@ -0,0 +1,20 @@
+package main
+
+//输出其命令行参数
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var s, sep string
+
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+
+	fmt.Println(s)
+
+}
