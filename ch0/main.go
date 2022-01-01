e58819fffd5b8ed466dc1979e4c00bfdd4580def
LuckilyChensy <LuckilyChensy@163.com>
2022-01-01 20:46:28 +0800

    map 初始化

--- ch0/main.go
+++ ch0/main.go
@@ -11,6 +11,7 @@ func main() {
 	}
 
 	fmt.Println(len(map01))
+	fmt.Println("------------------------end------------------------")
 
 	map02 := map[int]string{}
 
@@ -19,4 +20,29 @@ func main() {
 	}
 
 	fmt.Println(len(map02))
+	fmt.Println("------------------------end------------------------")
+
+	map03 := make(map[int]string)
+	if map03 == nil {
+		fmt.Println("map03 is nil")
+	}
+	fmt.Println(len(map03))
+	fmt.Println("------------------------end------------------------")
+
+	map04 := make(map[int]string, 10)
+	if map04 == nil {
+		fmt.Println("map04 is nil")
+	}
+	fmt.Println(len(map04))
+	fmt.Println("------------------------end------------------------")
+
+	map05 := make(map[int]string, 1)
+	if map05 == nil {
+		fmt.Println("map05 is nil")
+	}
+
+	map05[1] = "value01"
+	map05[2] = "value02"
+	map05[3] = "valye03"
+	fmt.Println("map05:", map05)
 }
