3a5cc4301fe8be9ff391b3a4d018d99e6a00baf7
LuckilyChensy <LuckilyChensy@163.com>
2022-01-01 20:24:23 +0800

    map 初始化

--- ch0/main.go
+++ ch0/main.go
@@ -7,8 +7,16 @@ func main() {
 	var map01 map[int]string
 
 	if map01 == nil {
-		fmt.Println("map is nil")
+		fmt.Println("map01 is nil")
 	}
 
 	fmt.Println(len(map01))
+
+	var map02 map[int]string
+
+	if map02 == nil {
+		fmt.Println("map02 is nil")
+	}
+
+	fmt.Println(len(map02))
 }
