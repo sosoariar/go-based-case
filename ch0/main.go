91d88fba4ede7e04035ccf4e41be846be362d2a8
LuckilyChensy <LuckilyChensy@163.com>
2022-01-01 20:14:04 +0800

    append 函数

--- ch0/main.go
+++ ch0/main.go
@@ -7,6 +7,10 @@ func main() {
 	arr := []int{1, 2, 3, 4, 5}
 	s := arr[1:3:5]
 
+	arr = append(arr, 888)
+	arr = append(arr, 888)
+	arr = append(arr, 888)
+	fmt.Println(arr)
 	fmt.Println("s = ", s)
 	fmt.Println(len(s))
 	fmt.Println(cap(s))
