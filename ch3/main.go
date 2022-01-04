d04b8f688d3776fb149f0602765413e9ee46409c
LuckilyChensy <LuckilyChensy@163.com>
2022-01-05 01:44:19 +0800

    Go程序设计语言 3.1 整数

---
+++ ch3/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fun01()
+}
+
+func fun01() {
+
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+	fmt.Printf("%08b\n", x)
+	fmt.Printf("%08b\n", y)
+
+}
