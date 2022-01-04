8f02dce92d95f4ba7791d834f9f5dfa356a6ac4b
LuckilyChensy <LuckilyChensy@163.com>
2022-01-04 16:04:26 +0800

    结构体初始化方式 1

---
+++ ch4/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+
+	// method1
+	p2 := Person{}
+	p2.Name = "Go语言"
+	p2.Age = 15
+	fmt.Println(p2)
+
+	p3 := Person{"Java语言", 25}
+	fmt.Println(p3)
+
+}
