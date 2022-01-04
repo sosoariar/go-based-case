fb23d7b4ec76cadfafc6fced31e104445fcdd8a8
LuckilyChensy <LuckilyChensy@163.com>
2022-01-05 01:49:11 +0800

    Go程序设计语言 3.1 整数

--- ch3/main.go
+++ ch3/main.go
@@ -15,4 +15,11 @@ func fun01() {
 
 	fmt.Printf("%08b\n", x&y)
 	fmt.Printf("%08b\n", x|y)
+	fmt.Printf("%08b\n", x^y)
+
+	for i := uint(0); i < 8; i++ {
+		if x&(1<<i) != 0 {
+			fmt.Println(i)
+		}
+	}
 }
