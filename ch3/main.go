5d16696eda96f4c4f10598edadff6105824c6c0a
LuckilyChensy <LuckilyChensy@163.com>
2022-01-06 00:51:11 +0800

    浮点数 1

--- ch3/main.go
+++ ch3/main.go
@@ -1,13 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const e = 2.71828
+const Avogadro = 6.02214129e23
+const Planck = 6.62606957e23
 
 func main() {
 	//fun01()
-	fun02()
+	//fun02()
+	fun03()
+}
+
+func fun03() {
+
+	fmt.Println(math.MaxFloat32)
+	fmt.Println(math.MaxFloat64)
+
+	var var1 float32 = 16777216
+	fmt.Println(var1 == var1+1)
+
+	for x := 0; x < 8; x++ {
+		fmt.Printf("x=%d e = %8.3f\n", x, math.Exp(float64(x)))
+	}
+
+	var z float64
+	fmt.Println(z, -z, 1/z, -1/z, z/z)
+
 }
 
 func fun02() {
+
 	var1 := 011
 	fmt.Printf("十进制 %d \n", var1)
 	fmt.Printf("二进制 %b \n", var1)
