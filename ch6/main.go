133813f739762e1118b15aa9b06919c42a1739d2
LuckilyChensy <LuckilyChensy@163.com>
2022-01-11 00:06:25 +0800

    init

--- ch6/main.go
+++ ch6/main.go
@@ -40,3 +40,16 @@ func Distance(p, q Point) float64 {
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
+
+type Path []Point
+
+// Distance 方法返回路径的长度
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
