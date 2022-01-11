6433af335a6ea470156cdc9e009ed0fca9a5200e
LuckilyChensy <LuckilyChensy@163.com>
2022-01-11 23:54:23 +0800

    变量类型的声明方式

--- ch3/main.go
+++ ch3/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	case01()
+}
+
+func case01() {
+
+	// 标准声明方式
+	var var1 string
+	var var2 int = 100 // 声明的同时指定初始值
+	var var3 bool
+	var var4 float64 = 100.0
+	var var5 byte
+	fmt.Println(var1, var2, var3, var4, var5)
+
+	// 批量声明方式
+	var (
+		var6 string
+		var7 int
+		var8 bool
+		var9 byte
+	)
+	fmt.Println(var6, var7, var8, var9)
 
 }
