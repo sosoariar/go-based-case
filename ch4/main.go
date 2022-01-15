61688393a2279cc600c8c1fdcd77c44b52f03aea
sosoariar <sosoariar@163.com>
2022-01-15 12:55:25 +0800

    数组声明

--- ch4/main.go
+++ ch4/main.go
@@ -25,13 +25,31 @@ type Employee struct {
 }
 
 func main() {
-	//fun01()
+	fun01()
 	//fun02()
 	//fun03()
 	//fun04()
 	//fun05()
 	//fun06()
-	fun07()
+	//fun07()
+}
+
+func fun01() {
+
+	var arr1 [3]int
+	var arr2 [3]int
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
+}
+
+func fun02() {
+
+	var1 := sha256.Sum256([]byte("x"))
+	var2 := sha256.Sum256([]byte("x"))
+	fmt.Printf("%x\n%x\n%t\n%T\n", var1, var2, var1 == var2, var1)
+
 }
 
 func fun07() {
@@ -84,34 +102,6 @@ func fun03() {
 	arr1 := []int{0, 1, 2, 3, 4, 5}
 	reverse(arr1[:])
 	fmt.Println(arr1)
-}
-
-func fun01() {
-
-	var arr1 [3]int
-
-	fmt.Println(arr1[0])
-	fmt.Println(arr1[len(arr1)-1])
-
-	var arr2 [3]int = [3]int{4, 5, 6}
-	for i, v := range arr2 {
-		fmt.Printf("index: %d , value: %d \n", i, v)
-	}
-
-	arr3 := [...]int{7, 8, 9, 10}
-	fmt.Printf("%T \n", arr3)
-	for i, v := range arr3 {
-		fmt.Printf("index: %d , value: %d \n", i, v)
-	}
-
-}
-
-func fun02() {
-
-	var1 := sha256.Sum256([]byte("x"))
-	var2 := sha256.Sum256([]byte("x"))
-	fmt.Printf("%x\n%x\n%t\n%T\n", var1, var2, var1 == var2, var1)
-
 }
 
 func reverse(s []int) {
